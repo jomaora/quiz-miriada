78488115e6095dac5ee85b796e76f2406039af10
Joan Ortega <jomaora@gmail.com>
2015-07-30 14:20:02 +0200

    Adding sqlite and sequelize schemas and local dbpath

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,8 +1,20 @@
+'use strict'
+
+var models = require('../models/models.js');
+
 exports.question = function(req, res, err) {
-	res.render('quizes/question', {pregunta: 'Capital de Italia?'});
+    models.Quiz.findAll()
+        .success(function(quizes) {
+            res.render('quizes/question', {pregunta: quizes[0].pregunta});
+        })
+    ;
 };
 
 exports.answer = function(req, res, err) {
-	var message = (req.query.respuesta === 'Roma') ? 'Correcto' : 'Incorrecto';
-	res.render('quizes/answer', {respuesta: message});	
+    models.Quiz.findAll()
+        .success(function(quizes) {
+            var message = (req.query.respuesta === quizes[0].respuesta) ? 'Correcto' : 'Incorrecto';
+            res.render('quizes/answer', {respuesta: message});
+        })
+    ;
 };
